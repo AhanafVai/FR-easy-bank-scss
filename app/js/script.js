c3d16445c6134057ba5d006b6c1758b6c209123e
ahanaf <ahanafabdullah9@gmail.com>
2022-10-12 20:45:18 +0600

    added overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,14 @@
 const hamburger = document.getElementById("header__cta");
+const overlay = document.getElementById("overlay");
 
 hamburger.addEventListener("click", () => {
   if (hamburger.classList.contains("header__hamburger")) {
     hamburger.classList.remove("header__hamburger");
     hamburger.classList.add("header__hamburger-close");
+    overlay.classList.remove("hidden");
   } else {
     hamburger.classList.remove("header__hamburger-close");
     hamburger.classList.add("header__hamburger");
+    overlay.classList.add("hidden");
   }
 });
