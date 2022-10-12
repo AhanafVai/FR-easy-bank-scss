a4941ada99533b8dba048ccb90f4873309cbd086
ahanaf <ahanafabdullah9@gmail.com>
2022-10-12 19:56:05 +0600

    added hamburger toggle option

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,11 @@
-console.log("hi there from console");
+const hamburger = document.getElementById("header__cta");
+
+hamburger.addEventListener("click", () => {
+  if (hamburger.classList.contains("header__hamburger")) {
+    hamburger.classList.remove("header__hamburger");
+    hamburger.classList.add("header__hamburger-close");
+  } else {
+    hamburger.classList.remove("header__hamburger-close");
+    hamburger.classList.add("header__hamburger");
+  }
+});
